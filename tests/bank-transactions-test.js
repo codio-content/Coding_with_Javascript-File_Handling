6dbe48dd9c995257b47819f73bd37a718d96bb37
codio-units <spackham@codio.com>
2015-11-09 20:06:07 +0000

    still finishing changes

--- tests/bank-transactions-test.js
+++ tests/bank-transactions-test.js
@@ -2,19 +2,13 @@ var fs= require('fs');
 var test = require('./test-fw.js');
 
 function copyFileToFile(src, dst){
-  var fsrc= fs.readFileSync(src, 'utf8');
-  var fdst= fs.writeFileSync(dst, fscr, 'utf8');
+  fs.writeFileSync(dst, fs.readFileSync(src, 'utf8'), 'utf8');
 }
 
 function loadAccounts(pathToAccounts){
   var text= fs.readFileSync(pathToAccounts, 'utf8')
-  var accounts= text.split("\n").map(function(n) { return n.split("|") })
-  accounts.rand= function(n){
-    if(this.length > 0){
-      return this.length % n;      
-    }
-    return -1;
-  }
+  var accounts= text.split("\n").map(function(n){ return n.split("|"); });
+  return accounts;
 }
 
 function loadTransactions(pathToTransactions){
@@ -27,44 +21,57 @@ function a2dToPipeDelim(a2d){
   return a2d.map(function(n,i){ return n.join("|"); }).join("\n");
 }
 
-function generateTestTransactions(pathToAccounts, pathToTrasactions){
+function generateTestTransactions(pathToAccounts, pathToTransactions){
   var result= [];
-  var accounts= loadAccounts(pathToAccounts)
+  var accounts= loadAccounts(pathToAccounts);
   if(accounts.length > 0){
-    var a= accounts.rand(7);  // semi random selection
-    result.push(['add', 1000, a[0], a[1]);     // add some
-    result.push(['add', 1000, a[0], a[1] + 1); // bad pin
+    var a= accounts[7 % accounts.length];        // semi random selection
+    result.push(['add', 1000, a[0], a[1]]);      // add some
+    result.push(['add', 1000, a[0], +a[1] + 1]); // bad pin
     
-    a= accounts.rand(5);
-    result.push(['sub', 0 - a[2], a[0], a[1]); // good sub
-    result.push(['sub', 10000, a[0], a[1]);    // bad sub
+    a= accounts[5 % accounts.length];            // semi random selection
+    result.push(['sub', a[2], a[0], a[1]]);      // good sub
+    result.push(['sub', 10000, a[0], a[1]]);     // bad sub
 
-    
-    a= accounts.rand(17);
+    a= accounts[17 % accounts.length];               // semi random selection
     result.push(['add', 1000, a[0], a[1]]);          // add some
     result.push(['add', 1000, a[0] + '9999', a[1]]); // bad acct
     
     result.push(['sub', a[2] + 100, a[0], a[1]]);    // bad sub
   }
   
-  var out= a2dToPipeDelim(accounts);
-  console.info("Gen TX:");
-  console.info(out);
-  fs.writeFileSync(pathToTransactions, out);
+  var out= a2dToPipeDelim(result);
+  //console.info("Gen TX:");
+  //console.info(out);
+  //console.info("--------");
+  
+  fs.writeFileSync(pathToTransactions, out, 'utf8');
 }
 
 function processTransaction(accounts, transaction){
-  for(var i=0; i < account.length; i++){
-    if(accounts[0] == transaction[2] && accounts[1] == transaction[3]){
+  for(var i=0; i < accounts.length; i++){
+    if(accounts[i][0] == transaction[2] && accounts[i][1] == transaction[3]){
+      //console.info(["TX", accounts[i], transaction])
       if(transaction[0] == 'add'){
-        accounts[2]= +accounts[2] + transaction[1]; 
+        accounts[i][2]= +accounts[i][2] + +transaction[1];
       } else if(transaction[0] == 'sub'){
-        if(+accounts[2] - transaction[1] > 0){
-          accounts[2]= +accounts[2] - transaction[1];
+        if(+accounts[i][2] - +transaction[1] >= 0){
+          accounts[i][2]= +accounts[i][2] - +transaction[1];
         }
+      } else {
+        //console.info("BAD ARG");
       }
+      //console.info(["POST", accounts[i]])
     }
   }
+  return accounts;
+}
+
+function processTransactions(accounts, transactions){
+  for(var i=0; i < transactions.length; i++){
+    accounts= processTransaction(accounts, transactions[i]);
+  }
+  return accounts;
 }
 
 function buildTest(pathAccounts, tmpFilename){
@@ -72,10 +79,9 @@ function buildTest(pathAccounts, tmpFilename){
   var F2= '/tmp/f2-' + tmpFilename;
   
   copyFileToFile(pathAccounts, F1);
-  generateTransactions(F1, F2);
+  generateTestTransactions(F1, F2);
   
-  accounts= loadAccounts(F1)
-  accounts= processTransactions(loadTransactions(F2));
+  var accounts= processTransactions(loadAccounts(F1), loadTransactions(F2));
 	var correctOutput= a2dToPipeDelim(accounts);
   
   return {
@@ -84,7 +90,7 @@ function buildTest(pathAccounts, tmpFilename){
     validate: function(i){
       var actualOutput= fs.readFileSync(i.inputs[0], "utf8").replace(/\s\s*$/, '');
       var resultAsExpected= correctOutput == actualOutput;
-      if(!resultAsExpected){
+      if(true || !resultAsExpected){
         console.info("ACTUAL\n" + actualOutput)
         console.info("CORRECT\n" + correctOutput)
         console.info("LOCAL: " + actualOutput.localeCompare(correctOutput))
